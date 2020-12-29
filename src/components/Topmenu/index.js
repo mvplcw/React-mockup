351b054088353f65b2adadf7b428ec13a7674aa1
mvplcw <limcheewei4727_@hotmail.com>
2020-12-29 19:29:52 +0800

    [FIX]collapse bar

--- src/components/Topmenu/index.js
+++ src/components/Topmenu/index.js
@@ -7,10 +7,10 @@ import './index.scss'
 
 const Topmenu = () => {
   return (
-    <div class="d-flex top-nav">
-        <div class="mr-auto p-2"><a><img src={logo} alt="logo"/></a></div>
-        <div class="d-inline p-2 centerhorizontally choose-location grey-text">Choose a city</div>
-        <div class="d-inline p-2 centerhorizontally grey-text"><FontAwesomeIcon icon={faBars} /></div>
+    <div className="d-flex top-nav">
+        <div className="mr-auto p-2"><a><img src={logo} alt="logo"/></a></div>
+        <div className="d-inline p-2 centerhorizontally choose-location grey-text">Choose a city</div>
+        <div className="d-inline p-2 centerhorizontally grey-text"><FontAwesomeIcon icon={faBars} /></div>
     </div>
   )
 }
